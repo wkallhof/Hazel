e3179f10bf2f8b4db9f0424c4a63d98f6af7c6f9
Wade Kallhoff <shagler@gmail.com>
2016-03-20 12:45:09 -0500

    Updating how TOC is generated

--- themes/default/public/scripts/page.js
+++ themes/default/public/scripts/page.js
@@ -1,9 +1,33 @@
+var SELECTORS = {
+    TOC_SELECTOR: ".toc",
+    TOC_CONTAINER: ".js-page",
+    TOC_SELECTORS: "h2, h3"
+};
+
+var DetailPage = function($scope) {
+    this.$tocSelector = $scope.find(SELECTORS.TOC_SELECTOR);
+    this.$tocContainer = $scope;
+    this.$tocSelectors = $scope.find(SELECTORS.TOC_SELECTORS);
+
+    this.init();
+};
+DetailPage.constructor = DetailPage;
+DetailPage.prototype = {
+    init: function() {
+        // build TOC
+        if (this.$tocSelector.length > 0 && this.$tocContainer.length > 0 && this.$tocSelectors.length > 0) {
+            this.$tocSelector.show();
+            this.$tocSelector.toc({
+                "container": SELECTORS.TOC_CONTAINER,
+                "selectors": SELECTORS.TOC_SELECTORS
+            });
+        }
+
+        // highlight code
+        hljs.initHighlightingOnLoad();
+    }
+};
+
 $(function() {
-    //Calls the tocify method on your HTML div.
-    $('.toc').toc({
-        "container": ".js-page",
-        "selectors": "h2,h3" //elements to use as headings
-    });
-    
-    hljs.initHighlightingOnLoad();
+    var page = new DetailPage($(SELECTORS.TOC_CONTAINER));
 });
