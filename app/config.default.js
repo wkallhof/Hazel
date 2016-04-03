2da43df5f074fc60abacbdc878746bc2e541ef41
Wade Kallhoff <shagler@gmail.com>
2016-04-02 19:03:04 -0500

    Updating package and config setup

--- app/config.default.js
+++ app/config.default.js
@@ -8,27 +8,20 @@ var config = {
     // Excerpt length (used in search)
     excerpt_length: 400,
 
-    // Which Theme to Use?
+    // Specify the theme to use
     theme_dir: __dirname + "/themes/",
     theme_name: "default",
 
-    // Specify the path of your content folder where all your '.md' files are located
-    // Fix: Needs trailing slash for now!
-    // Fix: Cannot be an absolute path
+    // Path in which to store content (markdown files, etc.)
     content_dir: __dirname + "/content/",
 
-    // Specify the path in which to store data (analytics, etc.)
+    // Path in which to store data (analytics, etc.)
     data_dir: __dirname + "/data/",
 
-    // Where is the public directory or document root?
+    // Path to the static file directory for themes
     public_dir: __dirname + "/themes/default/public/",
 
-    // The base URL of your images folder,
-    // Relative to config.public_dir
-    // (can use %image_url% in Markdown files)
-    image_url: "/images",
-
-    // Key used to sync two servers
+    // Secret key used to sync two servers
     sync_key: "",
 
     // Set to true to enable HTTP Basic Authentication
@@ -39,5 +32,4 @@ var config = {
     }
 };
 
-// Exports
 module.exports = config;
